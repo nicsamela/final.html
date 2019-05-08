e47ec51617285762cf86806833d6c9b4f4771385
nicsamela <46944902+nicsamela@users.noreply.github.com>
2019-05-08 09:56:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+  $('.tabs').tabs();
+  $('.datepicker').datepicker({
+    disableWeekends: true,
+    yearRange: 1
+  });
+  $('.tooltipped').tooltip();
+
+});
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+
+
+
+/*
+
+
+
+This is a JavaScript (JS) file.
+JavaScript is the programming language that powers the web.
+
+To use this file, place the following <script> tag just before the closing </body> tag in your HTML file, making sure that the filename after "src" matches the name of your file...
+
+    <script src="script.js"></script>
+
+Learn more about JavaScript at https://developer.mozilla.org/en-US/Learn/JavaScript
+
+When you're done, you can delete all of this grey text, it's just a comment.
+*/
